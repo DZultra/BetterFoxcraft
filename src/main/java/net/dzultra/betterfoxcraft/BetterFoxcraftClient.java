9afbc8ecfd3939d723ef545e8fc6c2d095c30965
DZultra <DanielZ123@web.de>
2024-12-19 14:17:43 +0100

    Added: - /imgur Command

--- src/main/java/net/dzultra/betterfoxcraft/BetterFoxcraftClient.java
+++ src/main/java/net/dzultra/betterfoxcraft/BetterFoxcraftClient.java
@@ -2,6 +2,7 @@ package net.dzultra.betterfoxcraft;
 
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
@@ -13,14 +14,16 @@ import net.minecraft.text.HoverEvent;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
-import net.minecraft.util.StringIdentifiable;
 import net.suuft.libretranslate.Language;
 import net.suuft.libretranslate.Translator;
+import okhttp3.*;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.stream.Stream;
 
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
 
@@ -93,8 +96,10 @@ public class BetterFoxcraftClient implements ClientModInitializer {
         ANSWER_MAP.put("store", "https://store.foxcraft.net/");
         ANSWER_MAP.put("forums", "https://www.mcfoxcraft.com/");
         ANSWER_MAP.put("vote", "https://www.mcfoxcraft.com/help/voting/");
-
     }
+
+    private static final String CLIENT_ID = "7ec76ddaf6350cc";
+    private static final String IMGUR_UPLOAD_URL = "https://api.imgur.com/3/image";
 
     @Override
     public void onInitializeClient() {
@@ -194,6 +199,96 @@ public class BetterFoxcraftClient implements ClientModInitializer {
         registerGamblingWarningCommand("rps");
         registerGamblingWarningCommand("jackpot");
         registerGamblingWarningCommand("cf");
+
+        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+            dispatcher.register(
+                    ClientCommandManager.literal("imgur")
+                            .then(ClientCommandManager.argument("title", StringArgumentType.string())
+                                    .then(ClientCommandManager.argument("description", StringArgumentType.greedyString())
+                                            .executes(context -> uploadScreenshot(context,
+                                                    StringArgumentType.getString(context, "title"),
+                                                    StringArgumentType.getString(context, "description")))))
+            );
+        });
+    }
+
+    private int uploadScreenshot(CommandContext<?> context, String title, String description) {
+        MinecraftClient client = MinecraftClient.getInstance();
+        Path screenshotsDir = client.runDirectory.toPath().resolve("screenshots");
+
+        // Find latest Screenshot
+        Optional<Path> latestScreenshot = getLatestScreenshot(screenshotsDir);
+        if (latestScreenshot.isEmpty()) {
+            client.execute(() -> client.player.sendMessage(Text.literal("No screeenshot found"), false));
+            return 1;
+        }
+
+        File imageFile = latestScreenshot.get().toFile();
+
+        new Thread(() -> {
+            try {
+                String response = uploadToImgur(imageFile, title, description);
+                client.execute(() -> client.player.sendMessage(
+                        Text.literal("Image succesfully uploaded: " + response)
+                                .setStyle(Style.EMPTY.withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, response)))
+                ));
+            } catch (IOException e) {
+                client.execute(() -> client.player.sendMessage(Text.literal("Error while uploading: " + e.getMessage()), false));
+                e.printStackTrace();
+            }
+        }).start();
+
+        return 1;
+    }
+    private Optional<Path> getLatestScreenshot(Path screenshotsDir) {
+        try (Stream<Path> files = Files.list(screenshotsDir)) {
+            return files
+                    .filter(Files::isRegularFile)
+                    .filter(file -> file.toString().endsWith(".png"))
+                    .max(Comparator.comparingLong(file -> file.toFile().lastModified()));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return Optional.empty();
+        }
+    }
+
+    private String uploadToImgur(File imageFile, String title, String description) throws IOException {
+        OkHttpClient client = new OkHttpClient.Builder()
+                .connectTimeout(30, java.util.concurrent.TimeUnit.SECONDS)
+                .writeTimeout(30, java.util.concurrent.TimeUnit.SECONDS)
+                .readTimeout(30, java.util.concurrent.TimeUnit.SECONDS)
+                .build();
+
+        byte[] imageBytes = Files.readAllBytes(imageFile.toPath());
+        String base64Image = java.util.Base64.getEncoder().encodeToString(imageBytes);
+
+        RequestBody body = new FormBody.Builder()
+                .add("image", base64Image)
+                .add("title", title)
+                .add("description", description)
+                .build();
+
+        Request request = new Request.Builder()
+                .url(IMGUR_UPLOAD_URL)
+                .addHeader("Authorization", "Client-ID " + CLIENT_ID)
+                .post(body)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                System.err.println("Response Code: " + response.code());
+                System.err.println("Response Body: " + response.body().string());
+                throw new IOException("Unexpectected code: " + response);
+            }
+            String responseBody = response.body().string();
+            return parseImgurLink(responseBody);}
+    }
+
+    private String parseImgurLink(String responseBody) {
+        String marker = "\"link\":\"";
+        int start = responseBody.indexOf(marker) + marker.length();
+        int end = responseBody.indexOf("\"", start);
+        return responseBody.substring(start, end);
     }
 
     private static void registerGamblingWarningCommand(String commandName) {
