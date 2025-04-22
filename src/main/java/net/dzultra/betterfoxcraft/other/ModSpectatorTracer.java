beadec486807e34ae07513dc47881cb00e4d284a
DZultra <DanielZ123@web.de>
2025-04-22 20:54:56 +0200

    Added Toggle for Spectator Tracer in Config

--- src/main/java/net/dzultra/betterfoxcraft/other/ModSpectatorTracer.java
+++ src/main/java/net/dzultra/betterfoxcraft/other/ModSpectatorTracer.java
@@ -1,5 +1,7 @@
 package net.dzultra.betterfoxcraft.other;
 
+import me.shedaniel.autoconfig.AutoConfig;
+import net.dzultra.betterfoxcraft.ModConfig;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.particle.ParticleTypes;
@@ -9,6 +11,7 @@ public class ModSpectatorTracer {
     public static void getSpectatorTracer() {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             if (client.world == null) return;
+            if (!AutoConfig.getConfigHolder(ModConfig.class).getConfig().enableSpectatorTracer) return;
 
             client.world.getEntities().forEach(entity -> {
                 if (entity instanceof PlayerEntity player && player.isSpectator()) {
