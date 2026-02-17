d33770468f88f2f43a5fab7cf30b7527be5d6387
DZultra <DanielZ123@web.de>
2026-02-17 14:52:52 +0100

    Fixed Git issue with KeybindHandler & ModKeyBinds

--- src/main/java/net/dzultra/betterfoxcraft/Keybinds/KeybindHandler.java
+++ src/main/java/net/dzultra/betterfoxcraft/Keybinds/KeybindHandler.java
@@ -1,15 +1,10 @@
-package net.dzultra.betterfoxcraft.Keybinds;
+package net.dzultra.betterfoxcraft.keybinds;
 
-import me.shedaniel.autoconfig.AutoConfig;
-import net.dzultra.betterfoxcraft.ModConfig;
-import net.dzultra.betterfoxcraft.slotswitcher.SlotSwitcher;
+import net.dzultra.betterfoxcraft.ConfigScreenFactory;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
-import net.fabricmc.fabric.api.client.screen.v1.ScreenKeyboardEvents;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.ingame.HandledScreen;
 
 @Environment(EnvType.CLIENT)
 public class KeybindHandler {
@@ -17,22 +12,9 @@ public class KeybindHandler {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (ModKeyBinds.openConfigKeybind.wasPressed()) {
                 if (client.player != null) {
-                    MinecraftClient.getInstance().setScreen(
-                            AutoConfig.getConfigScreen(ModConfig.class, MinecraftClient.getInstance().currentScreen).get()
-                    );
+                    MinecraftClient.getInstance().setScreen(ConfigScreenFactory.create(MinecraftClient.getInstance().currentScreen));
                 }
             }
-        });
-
-        ScreenEvents.BEFORE_INIT.register((client, screen, w, h) -> {
-            ScreenKeyboardEvents.afterKeyPress(screen).register((scr, key, scancode, modifiers) -> {
-                if (ModKeyBinds.moveKeybind.matchesKey(key, scancode) && scr instanceof HandledScreen<?> hs) {
-                    String title = hs.getTitle().getString();
-                    if (AutoConfig.getConfigHolder(ModConfig.class).getConfig().GUIName.equals(title)) {
-                        SlotSwitcher.scheduleConfiguredMoves(client, hs.getScreenHandler());
-                    }
-                }
-            });
         });
     }
 }
